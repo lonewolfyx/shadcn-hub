4dfbb563ef3e652efeef9608f66120ce88637512
lonewolfyx <877085893@qq.com>
2025-12-01 21:03:13 +0800

    refactor(pages): refactor the structure of the home page component, extract the page title and description as AppHeader component, and extract the application list item as RegisterApp component

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col justify-center gap-2">
+        <span class="font-mono font-medium text-xl leading-none">{{ title }}</span>
+        <div class="flex items-center gap-2 text-sm font-normal text-secondary-foreground">
+            {{ description }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{
+    title: string
+    description?: string
+}>(), {
+    description: '',
+})
+</script>
