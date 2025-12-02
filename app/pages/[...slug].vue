ed8444f513bffbdd56ab14b59d5d1f6ad72ebc48
lonewolfyx <877085893@qq.com>
2025-12-02 22:12:10 +0800

    feat(pages): dynamically render classified page content

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,13 +1,49 @@
 <template>
-    <div>
-        <slot />
+    <div class="flex flex-col">
+        <div class="flex flex-wrap items-center lg:items-end justify-between gap-5 py-4">
+            <AppHeader
+                :description="registryShadcnDirectory?.category.description"
+                :title="registryShadcnDirectory?.category.name ?? ''"
+            />
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <RegisterApp
+                :registries="registryShadcnDirectory?.registries ?? []"
+            />
+        </div>
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { categories } from '~/constant/categories'
+import type { ICategories } from '~/types/categories'
+import virtualShadcnHub from 'virtual:shadcn-hub'
+import type { IRegistryChannel } from '~/types/registry'
 
+const route = useRoute()
+
+const registryCategory = Object.fromEntries(
+    categories.map((item: ICategories) => [`/${item.url}`, { ...item }]),
+)
+
+const registryShadcnDirectory = computed(() => {
+    const registryItem = registryCategory[route.path] as ICategories
+
+    if (!registryItem) {
+        return null
+    }
+
+    return {
+        category: { ...registryItem },
+        registries: virtualShadcnHub[registryItem.name] as IRegistryChannel[],
+    }
+})
+
+if (!registryShadcnDirectory.value) {
+    throw createError({
+        statusCode: 404,
+        statusMessage: 'Page not found',
+        fatal: true,
+    })
+}
 </script>
-
-<style scoped>
-
-</style>
