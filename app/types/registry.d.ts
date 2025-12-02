ce0c4a44dc761c8e21129336e14ec848990ac347
lonewolfyx <877085893@qq.com>
2025-12-02 20:58:19 +0800

    feat(shadcn-hub): Added virtual module plugin support

---
+++ app/types/registry.d.ts
@@ -0,0 +1,39 @@
+export type CategoryType
+    = | 'Libraries & Components'
+        | 'Templates & Starters'
+        | 'Plugins & Tools'
+        | 'Design & Style Resources'
+        | 'IconFont'
+        | 'Shadcn Blocks'
+
+export interface IRegistryChannel {
+    name: string
+    slug: string
+    description: string
+    categories: CategoryType[]
+    tags: string[]
+    type: string
+    links: {
+        homepage: string
+        repository: string
+    }
+    status: 'active' | 'deprecated'
+    tech: {
+        framework: 'vue' | 'react'
+    }
+    author: {
+        name: string
+        github: string
+    }
+    github: {
+        star: number
+    }
+    added_at: string
+    updated_at: string
+}
+
+export interface IShadcnHubRegistryMap {
+    registries: IRegistryChannel[]
+
+    [key: string]: any
+}
---
+++ plugins/virtual-shadcn-hub.ts
@@ -0,0 +1,66 @@
+import type { Plugin } from 'vite'
+import { debounce } from 'radash'
+import { resolve } from 'node:path'
+import { readFile } from 'node:fs/promises'
+import type { CategoryType, IRegistryChannel, IShadcnHubRegistryMap } from '../app/types/registry'
+
+const ID = 'virtual:shadcn-hub'
+const VIRTUAL_ID = '\0' + ID
+const ROOT_PATH = resolve(process.cwd(), 'public')
+const SHADCN_HUB_REGISTRY_PATH = resolve(process.cwd(), 'public/shadcn-hub.json')
+
+const groupByCategories = async () => {
+    const registries = JSON.parse(await readFile(SHADCN_HUB_REGISTRY_PATH, 'utf-8')) as IShadcnHubRegistryMap
+
+    const result: Record<CategoryType, IRegistryChannel[]> = {
+        'Libraries & Components': [],
+        'Templates & Starters': [],
+        'Plugins & Tools': [],
+        'Design & Style Resources': [],
+        'IconFont': [],
+        'Shadcn Blocks': [],
+    }
+
+    for (const item of registries.registries) {
+        for (const category of item.categories) {
+            result[category].push(item)
+        }
+    }
+
+    return result
+}
+
+export const virtualShadcnHub = (): Plugin => {
+    let serverInstance: any
+
+    return {
+        name: 'vite-plugin-virtual-shadcn-hub',
+        resolveId(id) {
+            return id === ID ? VIRTUAL_ID : null
+        },
+        async load(id) {
+            if (id !== VIRTUAL_ID) return
+
+            console.log('shadcn hub load', await groupByCategories())
+            return `export default ${JSON.stringify(await groupByCategories())}`
+        },
+        configureServer(server) {
+            serverInstance = server
+
+            const onFileChange = debounce({ delay: 100 }, async (eventName: string, file: string) => {
+                if (!file.includes(ROOT_PATH)) return
+
+                console.log(`shadcn hub update`, file)
+
+                const mod = serverInstance.moduleGraph.getModuleById(VIRTUAL_ID)
+                if (mod) {
+                    serverInstance.moduleGraph.invalidateModule(mod)
+                    serverInstance.ws.send({ type: 'full-reload' })
+                }
+            })
+
+            serverInstance.watcher.add(ROOT_PATH)
+            serverInstance.watcher.on('all', onFileChange)
+        },
+    }
+}
